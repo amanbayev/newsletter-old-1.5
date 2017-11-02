3d52e8342dd8136c63c764a26b9c70b1fa6979ce
Talgat Amanbayev <amanbayev@gmail.com>
2017-11-02 16:29:46 +0600

    added bert

--- server/main.js
+++ server/main.js
@@ -5,4 +5,19 @@ Meteor.startup(() => {
   Meteor.publish("allUsers", function(){
     return Meteor.users.find();
   });
+
+  if ( Meteor.users.find().count() === 0 ) {
+    let cResult = Accounts.createUser({
+        username: 'admin',
+        email: 'amanbayev@gmail.com',
+        password: '4thebest',
+        profile: {
+            first_name: 'Talgat',
+            last_name: 'Amanbayev',
+            company: 'Kazakh Tourism',
+        }
+    });
+
+    Roles.addUsersToRoles(cResult, 'admin');
+  }
 });
