04a8fd08f4092693cee80f99de2079f73091b492
Talgat Amanbayev <amanbayev@gmail.com>
2017-11-03 09:47:39 +0600

    added react datepicker hacker01x1 and moment

--- client/imports/components/NewsletterComponent.jsx
+++ client/imports/components/NewsletterComponent.jsx
@@ -4,12 +4,48 @@ import { Link } from 'react-router-dom'
 export default class NewsletterComponent extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      creatingNewsletter: false
+    }
   }
   renderCreateButton() {
     if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
-      return (
-        <div><button className="btn btn-primary">Создать &nbsp;<i className="fa fa-plus"></i></button></div>
-      )
+      if (this.state.creatingNewsletter) {
+        return (
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h3 className="panel-title">Panel title</h3>
+            </div>
+            <div className="panel-body">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1">Password</label>
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile" />
+                  <p className="help-block">Example block-level help text here.</p>
+                </div>
+                <div className="checkbox">
+                  <label>
+                    <input type="checkbox" /> Check me out
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-default">Submit</button>
+              </form>
+            </div>
+          </div>
+        )
+      } else {
+        return (
+          <div><button className="btn btn-primary" onClick={()=>{ this.setState({creatingNewsletter: true})}}>Создать &nbsp;<i className="fa fa-plus"></i></button></div>
+        )
+      }
     } else {
       return (<div></div>)
     }
@@ -27,7 +63,6 @@ export default class NewsletterComponent extends Component {
     ))
   }
   render() {
-
     return (
       <div>
         <h1>Ньюслеттеры</h1>
